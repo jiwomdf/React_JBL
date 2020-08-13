54c82a7205967af395f2da418765ed9b68433a43
jiwomdf <mdf.jiwo@gmail.com>
2020-08-14 02:40:57 +0700

    add search bar

--- src/assets/style.js
+++ src/assets/style.js
@@ -1,31 +1,41 @@
-
 /* Navigation */
 export const navBtn = {
-    primary: "block inline-block mt-0 text-purple-500 hover:text-white mr-4"
-}
-export const btnDarkMode = "block inline-block mt-0 text-black-500 hover:text-white mr-4"
+  primary: "block inline-block mt-0 text-purple-500 hover:text-white mr-4",
+};
+export const btnDarkMode =
+  "block inline-block mt-0 text-black-500 hover:text-white mr-4";
+
+/* Bar */
+export const searchBar =
+  "text-gray-600 py-2 border border-transparent rounded-lg focus:outline-none hover:shadow-lg text-center shadow-md";
 
 /* Text Style */
-export const txtTitle = "flex justify-center pb-4 pt-4 text-5xl"
+export const txtTitle = "flex justify-center pb-4 pt-4 text-5xl";
 
 /* Label */
 export const label = {
-    small: "block uppercase tracking-wide text-xs font-bold mb-2",
-    medium: "block font-bold text-xl mb-2 text-sm font-bold mb-2"
-}
-export const labelContainer = "md:flex md:items-center mb-6"
+  small: "block uppercase tracking-wide text-xs font-bold mb-2",
+  medium: "block font-bold text-xl mb-2 text-sm font-bold mb-2",
+};
+export const labelContainer = "md:flex md:items-center mb-6";
 export const labelInput = {
-    small: "appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500",
-    medium: "appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-}
+  small:
+    "appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500",
+  medium:
+    "appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-purple-500",
+};
 
 /* Button */
 export const btn = {
-    primary: "shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded",
-    primary_rounded: "inline-block bg-purple-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 focus:shadow-outline focus:outline-none hover:bg-purple-400"
-}
+  primary:
+    "shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none hover:shadow-lg text-white font-bold py-2 px-4 rounded",
+  primary_rounded:
+    "inline-block bg-purple-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 focus:shadow-outline focus:outline-none hover:bg-purple-400",
+  green:
+    "shadow bg-green-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none hover:shadow-lg text-white font-bold py-2 px-4 rounded",
+};
 
 /* Form */
 export const form = {
-    card: "shadow-md rounded pt-8 pl-12 pr-12 pb-12"
-}
+  card: "shadow-md rounded pt-8 pl-12 pr-12 pb-12",
+};
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from '../contexts/ThemeContext';
-import { ImageListContext } from '../contexts/ImageListContext';
-import { btn, txtTitle } from '../assets/style'
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { ImageListContext } from "../contexts/ImageListContext";
+import { btn, txtTitle, searchBar } from "../assets/style";
 
 /* class BookList extends Component {
     static contextType = ThemeContext
@@ -21,48 +21,77 @@ import { btn, txtTitle } from '../assets/style'
 } */
 
 const BookList = () => {
-    const { isLightTheme, light, dark } = useContext(ThemeContext);
-    const { books } = useContext(ImageListContext)
-    const theme = isLightTheme ? light : dark
-    const content = (
-        <>
-            <p className={`${txtTitle} text-center hidden md:flex`}>Welcome to JBL Store</p>
-            <div className="grid gap-2 lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 container mx-auto">
-                {books.map(book => {
-                    return (
-                        <div key={book.id} style={{ background: theme.ui }} className="max-w-sm rounded overflow-hidden shadow-lg m-6" >
-                            <img src="https://source.unsplash.com/random/250x150" alt={book.id} className="w-full h-full" />
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(ImageListContext);
+  const theme = isLightTheme ? light : dark;
+  const content = (
+    <>
+      <p className={`${txtTitle} text-center hidden md:flex`}>
+        Welcome to JBL Store
+      </p>
 
-                            <div className="px-4 py-4">
-                                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                                <p className="text-gray-700 text-base">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            </div>
-                            <div className="px-4 py-4">
-                                <button className={btn.primary_rounded}>Detail</button>
-                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Rp. 25000</span>
-                            </div>
-                        </div>
-                    )
-                })}
+      {/* Search Bar */}
+      <div className="flex justify-center mt-2 mb-8">
+        <input type="text" placeholder="search" className={searchBar} style={{ width: 500 }} />
+        <button className={`${btn.primary} ml-2`}>Search</button>
+      </div>
+
+
+      <div className="grid gap-2 lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 container mx-auto" style={{ background: theme.con, borderRadius: 10 }}>
+        {books.map((book) => {
+          return (
+            <div
+              key={book.id}
+              style={{ background: theme.ui }}
+              className="max-w-sm rounded overflow-hidden shadow-lg m-6"
+            >
+              <img
+                src="https://source.unsplash.com/random/250x150"
+                alt={book.id}
+                className="w-full h-full"
+              />
+
+              <div className="px-4 py-4">
+                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </p>
+              </div>
+              <div className="px-4 py-4">
+                <button className={btn.primary_rounded}>Detail</button>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                  Rp. 25000
+                </span>
+              </div>
             </div>
-        </>
-    )
+          );
+        })}
+      </div>
+    </>
+  );
 
-    return (
-        <div>
-            {books.length <= 4 ?
-                <div style={{ color: theme.syntax, background: theme.bg, height: "100vh", minHeight: "100vh" }} >
-                    {content}
-                </div> :
-                <div style={{ color: theme.syntax, background: theme.bg, height: "100%" }} >
-                    {content}
-                </div>
-            }
-
-
+  return (
+    <div>
+      {books.length <= 4 ? (
+        <div
+          style={{
+            color: theme.syntax,
+            background: theme.bg,
+            height: "100vh",
+            minHeight: "100vh",
+          }}
+        >
+          {content}
         </div>
-    );
-}
+      ) : (
+          <div
+            style={{ color: theme.syntax, background: theme.bg, height: "100%" }}
+          >
+            {content}
+          </div>
+        )}
+    </div>
+  );
+};
 
 export default BookList;
